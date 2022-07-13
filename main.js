57e750e78868622b88fab678bf752c9372767949
leahmarke <lmarke@live.com>
2022-07-13 01:09:56 -0400

    need to get forEach working

--- main.js
+++ main.js
@@ -1,60 +1,68 @@
-// `.length`
-//  * Create an array called `cars` which consists of 4 different types of cars as String type. The first car type should be Ford.
-
+// 1: .length
 const cars = ["ford", "mercedes", "jeep", "volvo"];
 console.log(cars);
 let carsLength = cars.length;
 console.log(`The length of the cars array is ${carsLength}.`);
 
-// `.concat()`
-// * Create another array called `moreCars` with 4 more different types of cars. The last car type should be Honda.
-// * Use the `concat` method to combine the `cars` and `moreCars` arrays into another array called `totalCars`.
-
+// 2: .concat()
 const moreCars = ["BMW", "ford", "mitsubishi", "honda"];
 const totalCars = cars.concat(moreCars);
 console.log("this is totalCars:", totalCars);
 
-// `.indexOf()` and `.lastIndexOf()`
-//    * Use the `indexOf` method to console.log the index of `Honda` in `totalCars`.
-//    * Use the `lastIndexOf` method to console.log the index of `Ford` in `totalCars`.
-
+// 3: .indexOf() and .lastIndexOf()
 const honda = totalCars.indexOf("honda");
 const ford = totalCars.lastIndexOf("ford");
 console.log("index of honda:", honda);
 console.log("last index of ford:", ford);
 
-// `.join()`
-//    * Use the `join` method to convert the array `totalCars` into a string called `stringOfCars`.
-
+// 4: .join()
 const stringOfCars = totalCars.join();
 console.log(stringOfCars);
 
-// `.split()`
-//    * Use the `split` method to convert `stringOfCars` into an array called `carsFromString`.
-
+// 5: .split()
 const carsFromString1 = stringOfCars.split(" ");
 const carsFromString2 = stringOfCars.split(",");
 console.log(carsFromString1, carsFromString2);
 
-// `.reverse()`
-//    * Use the `reverse` method to create an array `carsInReverse` which is the array `totalCars` in reverse.
-
+// 6: .reverse()
 const carsInReverse = totalCars.reverse();
 console.log(carsInReverse);
 
-// sort
+// 7: .sort()
 const alphabetical = carsInReverse.sort();
 console.log(carsInReverse.indexOf("BMW"));
 
-// slice
+// 8: .slice()
 const pets = ["dog", "cat", "fish", "rabbit", "snake", "lizard", "bird"];
 const reptiles = pets.slice(4, 6);
 console.log(reptiles);
 
-// splice
+// 9: .splice()
 const removedReptiles = pets.splice(4, 2, "hamster");
 console.log(pets);
 
-// pop
+// 10: .pop()
 const removedPet = pets.pop();
 console.log(removedPet);
+
+// 11: .push()
+pets.push(removedPet);
+console.log(pets);
+
+// 12: .shift()
+const shiftedElement = pets.shift();
+console.log(shiftedElement, pets);
+
+// 13: .unshift()
+pets.unshift("turtle");
+console.log(pets);
+
+// 14: .forEach()
+const numbers = [23, 45, 0, 2, 8, 44, 100, 1, 3, 91, 34];
+const addTwo = (num, index, arr) => {
+  let num = numbers[" "];
+  return num + 2;
+};
+numbers.forEach(addTwo);
+console.log(numbers);
+console.log(addTwo);
